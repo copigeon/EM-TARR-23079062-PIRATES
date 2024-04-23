6a2dbc921dd61b98867c8ba9ded88d08216e3b04
copigeon <146360316+copigeon@users.noreply.github.com>
2024-04-23 22:50:34 +0100

    bottle issues

--- Assets/Scripts/PlayerActivate.cs
+++ Assets/Scripts/PlayerActivate.cs
@@ -6,19 +6,48 @@ public class PlayerActivate : MonoBehaviour
 {
     private bool PlayerInZone;
     public bool OnOff = false;
+    private Animator anim;
+
+
 
     private void Awake()
     {
         ///Debug.Log("awake");
+        anim = gameObject.GetComponent<Animator>();
     }
-
 
     private void Update()
     {
-        if (PlayerInZone && Input.GetKeyDown(KeyCode.E))
+        if (PlayerInZone && Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log(gameObject.name);
+            Debug.Log(this.gameObject.name);
+            if(OnOff == false)
+            {
+                anim.SetTrigger("Activate");
+                if (this.gameObject.name == "SodaStream")
+                {
+                    OnOff = true;
+                }
+            }
         }
+    }
+
+    private void OnEnd()
+    {
+        anim.Play("Dispense Bottle", 0, 787f);
+        anim.speed = 0f;
+        Vector3 oldpos = gameObject.transform.Find("Bottle").position;
+        Quaternion oldrot = gameObject.transform.Find("Bottle").rotation;
+        gameObject.transform.Find("Bottle").SetParent(null,true);
+        UnityEngine.GameObject.Find("Bottle").transform.SetLocalPositionAndRotation(oldpos, oldrot);
+        //UnityEngine.GameObject.Find("Bottle").transform.rotation = oldrot;
+        //UnityEngine.GameObject.Find("Bottle").transform.position = oldpos;
+
+
+        //GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        //foreach (object go in allObjects)
+        //{ print(go + " is an active object"); }
+
     }
 
     private void OnTriggerEnter(Collider other)
